dbc10e8a1793bbc04fd21bbb4bc473db9c3198f8
anhhuy007 <imanhhuy007@gmail.com>
2024-11-16 17:19:10 +0700

    feat[model]: add transaction model

---
+++ models/transaction.schema.js
@@ -0,0 +1,48 @@
+/* 
+    This file is used to define the schema for the transaction model.
+
+    Table Transaction {
+        id string [pk]
+        sellerId string
+        buyerId string
+        itemId string
+        price float
+        fee float
+        createdAt timestamp [default: `now()`]
+    }
+*/
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TransactionSchema = new Schema(
+  {
+    sellerId: {
+      type: String,
+      required: true,
+    },
+    buyerId: {
+      type: String,
+      required: true,
+    },
+    itemId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    fee: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: { createdAt: true, updatedAt: false },
+    collection: "Transaction",
+  }
+);
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
