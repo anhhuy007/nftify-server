c7a757ddeef719346dc46ac9f1febb47eaeb2e71
Akerman0509 <strayslugs0509@gmail.com>
2024-12-17 10:32:54 +0700

    feat[api]: format api return in auth and user controller

--- utils/helperFunc.js
+++ utils/helperFunc.js
@@ -97,6 +97,35 @@ function getPinataUrl(pinataUrl) {
     return "https://" + pinataUrl;
 }
 
+function handleResponse(success, msg, data, pagination = {}){
+    if (data.total && data.limit){
+        pagination  = {
+            total: data.total,
+            limit: data.limit,
+            page: data.page,
+            totalPages: data.totalPages,
+        }
+        const dataRes = {
+            pagination: pagination,
+            items: data.items
+        }
+        return {
+            success: success,
+            message: msg,
+            data: dataRes
+        }
+    }
+    return {
+        success: success,
+        message: msg,
+        data: data
+    }
+
+
+}
+
+
+
 module.exports = {
     randomDates,
     respondPOSTItem,
@@ -105,4 +134,5 @@ module.exports = {
     convertStampToNFTMeta,
     getIPFSUrl,
     getPinataUrl,
+    handleResponse
 };
